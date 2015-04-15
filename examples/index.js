f7623dba4f82e40b87edf818bac08d1014e0ba2e
tajo <vojtech@miksu.cz>
2015-04-15 23:40:16 +0400

    eslint added, cleaning

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var Portal = require('../lib/portal.js');
+var PseudoModal = require('./pseudomodal');
+var LoadingBar = require('./loadingbar');
 
 // Main React app component
 var App = React.createClass({
 
   getInitialState: function() {
-    return { isPortalOpened: false };
+    return {isPortalOpened: false};
   },
 
   render: function() {
@@ -18,7 +20,7 @@ var App = React.createClass({
         <a href="https://github.com/tajo/react-portal">https://github.com/tajo/react-portal</a>
         <p> </p>
 
-        <Portal openByClickOn={button1} closeOnEsc={true}>
+        <Portal closeOnEsc={true} openByClickOn={button1}>
           <PseudoModal>
             <h2>Pseudo Modal</h2>
             <p>This react component is appended to the document body.</p>
@@ -29,13 +31,13 @@ var App = React.createClass({
         </Portal>
 
         <button onClick={this.toggleLoadingBar}>
-          {this.state.isPortalOpened ? "Close the second portal" : "Open the second portal"}
+          {this.state.isPortalOpened ? 'Close the second portal' : 'Open the second portal'}
         </button>
         <Portal isOpened={this.state.isPortalOpened}>
           <LoadingBar />
         </Portal>
 
-        <Portal openByClickOn={button2} closeOnOutsideClick={true}>
+        <Portal closeOnOutsideClick={true} openByClickOn={button2}>
           <div style={{border: '1px solid black', margin: 10, padding: 10}}>
             <p>Click anywhere outside of this portal to close it.</p>
           </div>
@@ -46,37 +48,7 @@ var App = React.createClass({
   },
 
   toggleLoadingBar: function(e) {
-    this.setState({ isPortalOpened: !this.state.isPortalOpened });
-  }
-
-});
-
-// First ported component
-var PseudoModal = React.createClass({
-
-  render: function() {
-    return (
-      <div style={{border: '1px solid blue', margin: 10, padding: 10}}>
-        {this.props.children}
-        <p><button onClick={this.props.closePortal}>Close this</button></p>
-      </div>
-    );
-  }
-
-});
-
-// Second ported component
-var LoadingBar = React.createClass({
-
-  render: function() {
-    return (
-      <div style={{border: '1px solid green', margin: 10, padding: 10}}>
-        <p>This could be a loading bar...</p>
-        <p>This portal is <strong>opened by the prop</strong> <i>isOpened</i>.</p>
-        <p>... when <i>openByClickOn</i> is not enough.</p>
-        <p>Notice, that by default you cannot close this by ESC or an outside click.</p>
-      </div>
-    );
+    this.setState({isPortalOpened: !this.state.isPortalOpened});
   }
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
+/*eslint-disable no-console */
+
+'use strict';
+
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var streamify = require('gulp-streamify');
 var livereload = require('gulp-livereload');
-var plumber = require('gulp-plumber');
 var babelify = require('babelify');
 var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
   livereload.listen(35729);
@@ -22,7 +25,7 @@ gulp.task('default', function() {
   function rebundle() {
     return watcher
       .bundle()
-      .on("error", function (err) { console.log("Error : " + err.message); })
+      .on('error', function(err) { console.log('Error : ' + err.message); })
       .pipe(source('bundle.js'))
       .pipe(gulp.dest('./examples'))
       .pipe(livereload());
@@ -30,7 +33,14 @@ gulp.task('default', function() {
   return rebundle();
 });
 
-gulp.task('build', function(){
+gulp.task('eslint', function() {
+  return gulp.src(['lib/**/*.js', 'gulpfile.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('build', function() {
   return gulp.src('./lib/portal.js')
     .pipe(babel())
     .pipe(gulp.dest('./'));
