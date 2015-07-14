250f01b148477a249681158a278e514863bf8f4e
Nicolas Hery <hery.nicolas@gmail.com>
2015-07-14 19:09:56 -0400

    Add onOpen prop

---
+++ examples/absoluteposition.js
@@ -0,0 +1,34 @@
+var React = require('react');
+
+var AbsolutePosition = React.createClass({
+
+  propTypes: {
+    top: React.PropTypes.number,
+    left: React.PropTypes.number,
+    width: React.PropTypes.number,
+    closePortal: React.PropTypes.func
+  },
+
+  render: function() {
+    var style = {
+      position: 'absolute',
+      top: this.props.top,
+      left: this.props.left,
+      width: this.props.width,
+      border: '1px solid gray',
+      background: '#fff',
+      margin: 10,
+      padding: 10
+    };
+
+    return (
+      <div style={style}>
+        <p>This portal is positioned using the <i>openByClickOn</i> element's position, and the <i>onOpen</i> callback.</p>
+        <p>Click anywhere outside to close it.</p>
+      </div>
+    );
+  }
+
+});
+
+module.exports = AbsolutePosition;
