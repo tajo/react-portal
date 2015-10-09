187217d7fa34f8479f6f6f5953e6d3c097ce6828
tajo <vojtech@miksu.cz>
2015-10-08 22:48:28 -0400

    eslint rules updated & typos fixed

--- examples/index.js
+++ examples/index.js
@@ -17,6 +17,7 @@ export default class App extends React.Component {
   }
 
   onClose() {
+    /*eslint no-console: 0*/
     console.log('Portal closed');
   }
 
@@ -24,16 +25,16 @@ export default class App extends React.Component {
     const button1 = <button>Open portal with pseudo modal</button>;
     const button2 = <button>Another portal</button>;
     const button3 = (
-      <button onClick={(function(e) {
-          const bodyRect = document.body.getBoundingClientRect();
-          const targetRect = e.target.getBoundingClientRect();
-          this.setState({
-            isOpened: true,
-            top: targetRect.top - bodyRect.top,
-            left:  targetRect.left - bodyRect.left,
-            width: targetRect.width
-          });
-        }).bind(this)}>
+      <button onClick={((e) => {
+        const bodyRect = document.body.getBoundingClientRect();
+        const targetRect = e.target.getBoundingClientRect();
+        this.setState({
+          isOpened: true,
+          top: targetRect.top - bodyRect.top,
+          left:  targetRect.left - bodyRect.left,
+          width: targetRect.width
+        });
+      }).bind(this)}>
         {'Open portal on top of button'}
       </button>
     );
@@ -44,7 +45,7 @@ export default class App extends React.Component {
         <a href="https://github.com/tajo/react-portal">https://github.com/tajo/react-portal</a>
         <p> </p>
 
-        <Portal closeOnEsc={true} openByClickOn={button1} testProp={this.state.someValue}>
+        <Portal closeOnEsc openByClickOn={button1} testProp={this.state.someValue}>
           <PseudoModal>
             <h2>Pseudo Modal</h2>
             <p>This react component is appended to the document body.</p>
@@ -54,14 +55,14 @@ export default class App extends React.Component {
           </PseudoModal>
         </Portal>
 
-        <button onClick={this.toggleLoadingBar}>
+        <button onClick={(e) => this.toggleLoadingBar(e)}>
           {this.state.isPortalOpened ? 'Close the second portal' : 'Open the second portal'}
         </button>
         <Portal isOpened={this.state.isPortalOpened} testProp={this.state.someValue}>
           <LoadingBar />
         </Portal>
 
-        <Portal closeOnOutsideClick={true} openByClickOn={button2} onClose={this.onClose}>
+        <Portal closeOnOutsideClick onClose={this.onClose} openByClickOn={button2}>
           <div style={{border: '1px solid black', margin: 10, padding: 10}}>
             <p>Click anywhere outside of this portal to close it.</p>
           </div>
@@ -70,17 +71,17 @@ export default class App extends React.Component {
         <div className="absolutePosition">
           {button3}
           <Portal
-            closeOnOutsideClick={true}
+            closeOnOutsideClick
             isOpened={this.state.isOpened}
             onClose={(function() {
               this.setState({isOpened: false});
               this.onClose();
             }).bind(this)}>
-            <AbsolutePosition top={this.state.top} left={this.state.left} width={this.state.width} />
+            <AbsolutePosition left={this.state.left} top={this.state.top} width={this.state.width} />
           </Portal>
         </div>
 
-        <button onClick={this.changeValue}>
+        <button onClick={(e) => this.changeValue(e)}>
           Change randomly value: {this.state.someValue}
         </button>
 
