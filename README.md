f5c75a288a34e79545360a914830f1eea70e8679
Samuel Reed <samuel.trace.reed@gmail.com>
2017-05-11 16:52:40 -0500

    Initial commit of react-portal-minimal.

--- README.md
+++ README.md
@@ -1,46 +1,38 @@
-React-portal
+React-Portal-Minimal
 ============
-[![npm version](https://img.shields.io/npm/v/react-portal.svg?style=flat-square)](https://www.npmjs.com/package/react-portal)
-[![npm downloads](https://img.shields.io/npm/dm/react-portal.svg?style=flat-square)](https://www.npmjs.com/package/react-portal)
-[![Build Status](https://travis-ci.org/tajo/react-portal.svg?branch=master)](https://travis-ci.org/tajo/react-portal)
+[![npm version](https://img.shields.io/npm/v/react-portal-minimal.svg?style=flat-square)](https://www.npmjs.com/package/react-portal-minimal)
+[![npm downloads](https://img.shields.io/npm/dm/react-portal-minimal.svg?style=flat-square)](https://www.npmjs.com/package/react-portal-minimal)
+[![Build Status](https://travis-ci.org/strml/react-portal-minimal.svg?branch=master)](https://travis-ci.org/strml/react-portal-minimal)
 
-> Struggling with modals, lightboxes or loading bars in React? React-portal creates a new top-level React tree and injects its child into it. That's necessary for proper styling (especially positioning).
+React-Portal-Minimal is an extremely minimal version of [React-Portal](https://github.com/tajo/react-portal).
+
+Compared to React-Portal, it is less than 1/3 the size, has minimal options, and no state.
+
+It is intended as a building block. It does only three things:
+
+1. Hoists its contents to a new React subtree.
+2. Optionally sets a `className` on the subtree root.
+3. Updates that `className` on the subtree root if it changes.
 
 ## Features
 
-- transports its child into a new React component and appends it to the **document.body** (creates a new independent React tree)
-- can be opened by the prop **isOpen**
-- can be opened after a click on an element that you pass through the prop **openByClickOn** (and then it takes care of the open/close state)
-- doesn't leave any mess in DOM after closing
-- provides its child with **this.props.closePortal** callback
-- provides **close on ESC** and **close on outside mouse click** out of the box
-- supports absolute positioned components (great for tooltips)
+- Transports its child into a new React component and appends it to the **document.body** (creates a new independent React tree)
+- Doesn't leave any mess in DOM after unmount.
 - **no dependencies**
 - **fully covered by tests**
-
-## Demo
-
-Try [https://miksu.cz/react-portal](https://miksu.cz/react-portal) **or**
-
-```shell
-git clone https://github.com/tajo/react-portal
-cd react-portal
-npm install
-npm run build:examples
-open examples/index.html
-```
 
 ## Installation
 
 ```shell
-npm install react react-dom react-portal --save
+npm install react react-dom react-portal-minimal --save
 ```
 
 ## Usage
+
 ```jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Portal from 'react-portal';
+import Portal from 'react-portal-minimal';
 
 export default class App extends React.Component {
 
@@ -48,7 +40,7 @@ export default class App extends React.Component {
     const button1 = <button>Open portal with pseudo modal</button>;
 
     return (
-      <Portal closeOnEsc closeOnOutsideClick openByClickOn={button1}>
+      <Portal>
         <PseudoModal>
           <h2>Pseudo Modal</h2>
           <p>This react component is appended to the document body.</p>
@@ -74,92 +66,26 @@ export class PseudoModal extends React.Component {
 
 ReactDOM.render(<App />, document.getElementById('react-body'));
 ```
+
 ## Documentation - props
 
-### Always required
+### Required
 
 #### children : ReactElement
 The portal expects one child (`<Portal><Child ... /></Portal>`) that will be ported.
 
-### One of these two required
-
-#### isOpen : bool
-*The V3 version uses `isOpened`. Renaming to `isOpen` is going to be released soon with the V4.*
-
-If true, the portal is open. If false, the portal is closed. It's up to you to take care of the closing (aka taking care of the state). Don't use this prop if you want to make your life easier. Use openByClickOn instead!
-
-#### openByClickOn : ReactElement
-The second way how to open the portal. This element will be rendered by the portal immediately
-with `onClick` handler that triggers portal opening. **How to close the portal then?** The portal provides its ported child with a callback `this.props.closePortal`. Or you can use built-in portal closing methods (closeOnEsc, ... more below). Notice that you don't have to deal with the open/close state (like when using the `isOpen` prop).
-
 ### Optional
 
-#### closeOnEsc: bool
-If true, the portal can be closed by the key ESC.
-
-#### closeOnOutsideClick: bool
-If true, the portal can be closed by the outside mouse click.
-
-#### onOpen: func(DOMNode)
-This callback is called when the portal is opened and rendered (useful for animating the DOMNode).
-
-#### beforeClose: func(DOMNode, removeFromDOM)
-This callback is called when the closing event is triggered but it prevents normal removal from the DOM. So, you can do some DOMNode animation first and then call removeFromDOM() that removes the portal from DOM.
-
-#### onClose: func
-This callback is called when the portal closes and after beforeClose.
-
-#### onUpdate: func
-This callback is called when the portal is (re)rendered.
-
-
-## Tips & Tricks
-- Does your modal have a fullscreen overlay and the `closeOnOutsideClick` doesn't work? [There is a simple solution](https://github.com/tajo/react-portal/issues/2#issuecomment-92058826).
-- Does your inner inner component `<LevelTwo />`
-
-```jsx
-<Portal>
-  <LevelOne>
-    <LevelTwo />
-  </LevelOne>
-</Portal>
-```
-
-also need an access to `this.props.closePortal()`? You can't just use `{this.props.children}` in render method of `<LevelOne>` component. You have to clone it instead:
-
-```jsx
-{React.cloneElement(
-  this.props.children,
-  {closePortal: this.props.closePortal}
-)}
-```
-
-#### Open modal programmatically
-
-Sometimes you need to open your portal (e.g. modal) automatically. There is no button to click on. No problem, because the portal has the `isOpen` prop, so you can just set it to `true` or `false`. However, then it's completely up to you to take care about the portal closing (ESC, outside click, no `this.props.closePortal` callback...).
-
-However, there is a nice trick how to make this happen even without `isOpen`:
-
-```jsx
-<Portal ref="myPortal">
-  <PseudoModal title="My modal">
-    Modal content
-  </PseudoModal>
-</Portal>
-```
-
-```jsx
-this.refs.myPortal.openPortal()
-// opens the portal, yay!
-```
+#### className: string
+A className to apply to the new React tree's root.
 
 ## Contribution
 
 Please, create issues and pull requests.
 
 ```shell
-git clone https://github.com/tajo/react-portal
-cd react-portal
+git clone https://github.com/strml/react-portal-minimal
+cd react-portal-minimal
 npm install
 npm start
 open http://localhost:3000
@@ -173,6 +99,6 @@ npm test
 
 ## Credits
 
-Inspired by the talk [React.js Conf 2015 - Hype!, Ryan Florence](https://www.youtube.com/watch?v=z5e7kWSHWTg)
+This project is based on @tajo's [react-portal](https://github.com/tajo/react-portal).
 
-Vojtech Miksu 2015, [miksu.cz](https://miksu.cz), [@vmiksu](https://twitter.com/vmiksu)
+That project: Vojtech Miksu 2015, [miksu.cz](https://miksu.cz), [@vmiksu](https://twitter.com/vmiksu)
--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Portal from '../lib/portal.js';
+import Portal from '../lib/portal';
 import PseudoModal from './pseudomodal';
 import LoadingBar from './loadingbar';
 import AbsolutePosition from './absoluteposition';
+import DocumentEvents from 'react-document-events';
 import TWEEN from 'tween.js';
+
+const buttonStyles = {
+  padding: 10,
+  fontSize: 20,
+  marginBottom: 10,
+};
 
 // Main React app component
 export default class App extends React.Component {
@@ -12,17 +19,22 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isPortalOpened: false,
-      someValue: 'init',
+      portal1Open: false,
+      portal2Open: false,
+      portal3Open: false,
+      portal4Open: false,
+      portal5Open: false,
     };
   }
 
-  onClose() {
-    /* eslint no-console: 0 */
-    console.log('Portal closed');
+  componentDidMount() {
+    requestAnimationFrame(function animate(time) {
+      TWEEN.update(time);
+      requestAnimationFrame(animate);
+    });
   }
 
-  onOpen(node) {
+  onAnimOpen(node) {
     new TWEEN.Tween({ opacity: 0 })
       .to({ opacity: 1 }, 500)
       .easing(TWEEN.Easing.Cubic.In)
@@ -32,120 +44,168 @@ export default class App extends React.Component {
       .start();
   }
 
-  beforeClose(node, removeFromDom) {
+  onAnimClose(node, cb) {
     new TWEEN.Tween({ opacity: 1 })
       .to({ opacity: 0 }, 500)
       .easing(TWEEN.Easing.Cubic.In)
       .onUpdate(function() {  // eslint-disable-line
         node.style.opacity = this.opacity; // eslint-disable-line
       })
-      .onComplete(removeFromDom)
+      .onComplete(cb)
       .start();
   }
 
-  toggleLoadingBar() {
-    this.setState({ isPortalOpened: !this.state.isPortalOpened });
+  onPortal4Click(e) {
+    if (!e) return this.setState({ portal4Open: !this.state.portal4Open });
+    const bodyRect = document.body.getBoundingClientRect();
+    const targetRect = e.target.getBoundingClientRect();
+    this.setState({
+      portal4Open: !this.state.portal4Open,
+      top: targetRect.top - bodyRect.top,
+      left: targetRect.left - bodyRect.left,
+      width: targetRect.width,
+    });
+    return null;
   }
 
-  changeValue() {
-    this.setState({ someValue: Math.random().toString(36).substring(7) });
+  onPortal5Click() {
+    const { portal5Open } = this.state;
+    if (!portal5Open) {
+      // Opening
+      this.setState({ portal5Open: true }, () => {
+        const node = this.portal5;
+        if (!node) return;
+        this.onAnimOpen(this.portal5);
+      });
+    } else {
+      // Closing
+      const node = this.portal5;
+      if (!node) return;
+      this.onAnimClose(node, () => {
+        this.setState({ portal5Open: false });
+      });
+    }
+  }
+
+  // Simulate click events from old lib
+  closeOnClickOutside(e, idx) {
+    const node = this[`portal${idx}`];
+    if (!node) return;
+    let el = e.target;
+    do { // eslint-disable-line no-cond-assign
+      if (el === node) return;
+    } while (el = el.parentNode);
+    this.togglePortal(idx);
+  }
+
+  closeOnEsc(e, idx) {
+    return e.keyCode === 27 && this.togglePortal(idx);
+  }
+
+  togglePortal(idx, cb) {
+    if (this[`onPortal${idx}Click`]) return this[`onPortal${idx}Click`]();
+    const key = `portal${idx}Open`;
+    this.setState({ [key]: !this.state[key] }, cb);
+    return null;
   }
 
   render() {
-    const buttonStyles = {
-      padding: 10,
-      fontSize: 20,
-      marginBottom: 10,
-    };
-    function animate(time) {
-      requestAnimationFrame(animate);
-      TWEEN.update(time);
-    }
-    requestAnimationFrame(animate);
-
-    const button1 = <button style={buttonStyles}>Open portal with pseudo modal</button>;
-    const button2 = <button style={buttonStyles}>Another portal</button>;
-    const button3 = (
-      <button
-        onClick={(e) => {
-          const bodyRect = document.body.getBoundingClientRect();
-          const targetRect = e.target.getBoundingClientRect();
-          this.setState({
-            isOpen: true,
-            top: targetRect.top - bodyRect.top,
-            left: targetRect.left - bodyRect.left,
-            width: targetRect.width,
-          });
-        }}
-        style={buttonStyles}
-      >
-        {'Open portal on top of button'}
-      </button>
-    );
-    const button4 = <button style={buttonStyles}>Animation Example</button>;
+    const { portal1Open, portal2Open, portal3Open, portal4Open, portal5Open } = this.state;
 
     return (
       <div style={{ border: '2px solid red', margin: 10, padding: 10 }}>
         <h1>React portal examples</h1>
-        <a href="https://github.com/tajo/react-portal">https://github.com/tajo/react-portal</a>
-        <p> </p>
+        <a href="https://github.com/strml/react-portal-minimal">https://github.com/strml/react-portal-minimal</a>
+        <p />
 
-        <Portal closeOnEsc openByClickOn={button1} testProp={this.state.someValue}>
-          <PseudoModal>
-            <h2>Pseudo Modal</h2>
-            <p>This react component is appended to the document body.</p>
-            <p>This is <strong>great for a modal, lightbox, loading bar ... etc.</strong>.</p>
-            <p>Close this by pressing <strong>ESC</strong>.</p>
-            <p><strong>Why psuedo?</strong> Because the proper CSS styles are up to you. ;-)</p>
-          </PseudoModal>
-        </Portal>
-
-        <button onClick={(e) => this.toggleLoadingBar(e)} style={buttonStyles}>
-          {this.state.isPortalOpened ? 'Close the second portal' : 'Open the second portal'}
-        </button>
-        <Portal isOpen={this.state.isPortalOpened} testProp={this.state.someValue}>
-          <LoadingBar />
-        </Portal>
-
-        <Portal closeOnOutsideClick onClose={this.onClose} openByClickOn={button2}>
-          <div style={{ border: '1px solid black', margin: 10, padding: 10 }}>
-            <p>Click anywhere outside of this portal to close it.</p>
-          </div>
-        </Portal>
-
-        <div>
-          {button3}
-          <Portal
-            closeOnOutsideClick
-            isOpen={this.state.isOpen}
-            onClose={() => { this.setState({ isOpen: false }); this.onClose(); }}
-          >
-            <AbsolutePosition
-              left={this.state.left}
-              top={this.state.top}
-              width={this.state.width}
-            />
+        {/* 1 */}
+        {portal1Open ?
+          <Portal>
+            <PseudoModal closePortal={() => this.togglePortal(1)}>
+              <DocumentEvents onKeyDown={(e) => this.closeOnEsc(e, 1)} />
+              <h2>Pseudo Modal</h2>
+              <p>This react component is appended to the document body.</p>
+              <p>This is <strong>great for a modal, lightbox, loading bar ... etc.</strong>.</p>
+              <p>Close this by pressing <strong>ESC</strong>.</p>
+              <p><strong>Why psuedo?</strong> Because the proper CSS styles are up to you. ;-)</p>
+            </PseudoModal>
           </Portal>
+        : null}
+        <button
+          style={buttonStyles}
+          onClick={() => this.togglePortal(1)}
+        >
+          Open portal with pseudo modal
+        </button>
+
+        {/* 2 */}
+        <button onClick={() => this.togglePortal(2)} style={buttonStyles}>
+          {portal2Open ? 'Close the second portal' : 'Open the second portal'}
+        </button>
+        {portal2Open ?
+          <Portal>
+            <LoadingBar />
+          </Portal>
+        : null}
+
+        {/* 3 */}
+        {portal3Open ?
+          <Portal>
+            <div
+              style={{ border: '1px solid black', margin: 10, padding: 10 }}
+              ref={(c) => (this.portal3 = c)}
+            >
+              <DocumentEvents onClick={(e) => this.closeOnClickOutside(e, 3)} />
+              <p>Click anywhere outside of this portal to close it.</p>
+            </div>
+          </Portal>
+        : null}
+        <button style={buttonStyles} onClick={() => this.togglePortal(3)}>Another portal</button>
+
+        {/* 4 */}
+        <div>
+          <button onClick={(e) => this.onPortal4Click(e)} style={buttonStyles}>
+            Open portal on top of button
+          </button>
+          {portal4Open ?
+            <Portal>
+              <span ref={(c) => (this.portal4 = c)}>
+                <DocumentEvents onClick={(e) => this.closeOnClickOutside(e, 4)} />
+                <AbsolutePosition
+                  left={this.state.left}
+                  top={this.state.top}
+                  width={this.state.width}
+                />
+              </span>
+            </Portal>
+          : null}
         </div>
 
-        <button onClick={(e) => this.changeValue(e)} style={buttonStyles}>
-          Change randomly value: {this.state.someValue}
-        </button>
-
-        <Portal
-          beforeClose={this.beforeClose}
-          closeOnEsc
-          closeOnOutsideClick
-          onOpen={this.onOpen}
-          openByClickOn={button4}
-          style={{ opacity: 0 }}
+        {/* 5 */}
+        {portal5Open ?
+          <Portal>
+            <div
+              style={{ border: '1px solid black', margin: 10, padding: 10 }}
+              ref={(c) => (this.portal5 = c)}
+            >
+              <DocumentEvents
+                onClick={(e) => this.closeOnClickOutside(e, 5)}
+                onKeyDown={(e) => this.closeOnEsc(e, 5)}
+              />
+              <p>
+                Trigger Animations, or any arbitrary function,
+                before removing the portal from the DOM.
+                The portal will animate out on both click outside and on ESC press.
+              </p>
+            </div>
+          </Portal>
+        : null}
+        <button
+          style={buttonStyles}
+          onClick={() => this.onPortal5Click()}
         >
-          <div style={{ border: '1px solid black', margin: 10, padding: 10 }}>
-            <p>Trigger Animations, or any arbitrary function,{' '}
-            before removing the portal from the DOM, animates out{' '}
-            on both click outside and on esc press</p>
-          </div>
-        </Portal>
+          Animation Example
+        </button>
       </div>
     );
   }
