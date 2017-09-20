206214fd24c5eac62d4fe1246fcc5b98e1fb9bb2
Anton Prohorov <fabledva@mail.ru>
2017-09-20 18:31:13 +0400

    Add more ES6 for repetitive props and states

--- src/portal.js
+++ src/portal.js
@@ -3,187 +3,194 @@ import ReactDOM, { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
 
 const KEYCODES = {
-  ESCAPE: 27
+ESCAPE: 27
 };
 
 export default class Portal extends React.Component {
-  constructor() {
-    super();
-    this.state = { active: false };
-    this.handleWrapperClick = this.handleWrapperClick.bind(this);
-    this.closePortal = this.closePortal.bind(this);
-    this.handleOutsideMouseClick = this.handleOutsideMouseClick.bind(this);
-    this.handleKeydown = this.handleKeydown.bind(this);
-    this.portal = null;
-    this.node = null;
-  }
+constructor() {
+super();
+this.state = { active: false };
+this.handleWrapperClick = this.handleWrapperClick.bind(this);
+this.closePortal = this.closePortal.bind(this);
+this.handleOutsideMouseClick = this.handleOutsideMouseClick.bind(this);
+this.handleKeydown = this.handleKeydown.bind(this);
+this.portal = null;
+this.node = null;
+}
 
-  componentDidMount() {
-    if (this.props.closeOnEsc) {
-      document.addEventListener('keydown', this.handleKeydown);
-    }
+componentDidMount() {
+const { closeOnEsc, closeOnOutsideClick, isOpen } = this.props;
+if (closeOnEsc) {
+document.addEventListener('keydown', this.handleKeydown);
+}
 
-    if (this.props.closeOnOutsideClick) {
-      document.addEventListener('mouseup', this.handleOutsideMouseClick);
-      document.addEventListener('touchstart', this.handleOutsideMouseClick);
-    }
+if (closeOnOutsideClick) {
+document.addEventListener('mouseup', this.handleOutsideMouseClick);
+document.addEventListener('touchstart', this.handleOutsideMouseClick);
+}
 
-    if (this.props.isOpen) {
-      this.openPortal();
-    }
-  }
+if (isOpen) {
+this.openPortal();
+}
+}
 
-  componentWillReceiveProps(newProps) {
-    // portal's 'is open' state is handled through the prop isOpen
-    if (typeof newProps.isOpen !== 'undefined') {
-      if (newProps.isOpen) {
-        if (this.state.active) {
-          this.renderPortal(newProps);
-        } else {
-          this.openPortal(newProps);
-        }
-      }
-      if (!newProps.isOpen && this.state.active) {
-        this.closePortal();
-      }
-    }
+componentWillReceiveProps(newProps) {
+// portal's 'is open' state is handled through the prop isOpen
+const { active } = this.state;
+if (typeof newProps.isOpen !== 'undefined') {
+if (newProps.isOpen) {
+if (active) {
+this.renderPortal(newProps);
+} else {
+this.openPortal(newProps);
+}
+}
+if (!newProps.isOpen && active) {
+this.closePortal();
+}
+}
 
-    // portal handles its own 'is open' state
-    if (typeof newProps.isOpen === 'undefined' && this.state.active) {
-      this.renderPortal(newProps);
-    }
-  }
+// portal handles its own 'is open' state
+if (typeof newProps.isOpen === 'undefined' && this.state.active) {
+this.renderPortal(newProps);
+}
+}
 
-  componentWillUnmount() {
-    if (this.props.closeOnEsc) {
-      document.removeEventListener('keydown', this.handleKeydown);
-    }
+componentWillUnmount() {
+if (this.props.closeOnEsc) {
+document.removeEventListener('keydown', this.handleKeydown);
+}
 
-    if (this.props.closeOnOutsideClick) {
-      document.removeEventListener('mouseup', this.handleOutsideMouseClick);
-      document.removeEventListener('touchstart', this.handleOutsideMouseClick);
-    }
+if (this.props.closeOnOutsideClick) {
+document.removeEventListener('mouseup', this.handleOutsideMouseClick);
+document.removeEventListener('touchstart', this.handleOutsideMouseClick);
+}
 
-    this.closePortal(true);
-  }
+this.closePortal(true);
+}
 
-  handleWrapperClick(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    if (this.state.active) {
-      return;
-    }
-    this.openPortal();
-  }
+handleWrapperClick(e) {
+e.preventDefault();
+e.stopPropagation();
+if (this.state.active) {
+return;
+}
+this.openPortal();
+}
 
-  openPortal(props = this.props) {
-    this.setState({ active: true });
-    this.renderPortal(props, true);
-  }
+openPortal(props = this.props) {
+this.setState({ active: true });
+this.renderPortal(props, true);
+}
 
-  closePortal(isUnmounted = false) {
-    const resetPortalState = () => {
-      if (this.node) {
-        ReactDOM.unmountComponentAtNode(this.node);
-        document.body.removeChild(this.node);
-      }
-      this.portal = null;
-      this.node = null;
-      if (isUnmounted !== true) {
-        this.setState({ active: false });
-      }
-    };
+closePortal(isUnmounted = false) {
+const { active } = this.state;
+const { beforeClose, onClose } = this.props;
+const resetPortalState = () => {
+if (this.node) {
+ReactDOM.unmountComponentAtNode(this.node);
+document.body.removeChild(this.node);
+}
+this.portal = null;
+this.node = null;
+if (isUnmounted !== true) {
+this.setState({ active: false });
+}
+};
 
-    if (this.state.active) {
-      if (this.props.beforeClose) {
-        this.props.beforeClose(this.node, resetPortalState);
-      } else {
-        resetPortalState();
-      }
+if (active) {
+if (beforeClose) {
+beforeClose(this.node, resetPortalState);
+} else {
+resetPortalState();
+}
 
-      this.props.onClose();
-    }
-  }
+onClose();
+}
+}
 
-  handleOutsideMouseClick(e) {
-    if (!this.state.active) {
-      return;
-    }
+handleOutsideMouseClick(e) {
+if (!this.state.active) {
+return;
+}
 
-    const root = findDOMNode(this.portal);
-    if (root.contains(e.target) || (e.button && e.button !== 0)) {
-      return;
-    }
+const root = findDOMNode(this.portal);
+if (root.contains(e.target) || (e.button && e.button !== 0)) {
+return;
+}
 
-    e.stopPropagation();
-    this.closePortal();
-  }
+e.stopPropagation();
+this.closePortal();
+}
 
-  handleKeydown(e) {
-    if (e.keyCode === KEYCODES.ESCAPE && this.state.active) {
-      this.closePortal();
-    }
-  }
+handleKeydown(e) {
+if (e.keyCode === KEYCODES.ESCAPE && this.state.active) {
+this.closePortal();
+}
+}
 
-  renderPortal(props, isOpening) {
-    if (!this.node) {
-      this.node = document.createElement('div');
-      document.body.appendChild(this.node);
-    }
+renderPortal(props, isOpening) {
+const { closePortal } = this;
+const { onOpen, onUpdate } = this.props;
+if (!this.node) {
+this.node = document.createElement('div');
+document.body.appendChild(this.node);
+}
 
-    if (isOpening) {
-      this.props.onOpen(this.node);
-    }
+if (isOpening) {
+onOpen(this.node);
+}
 
-    let children = props.children;
-    // https://gist.github.com/jimfb/d99e0678e9da715ccf6454961ef04d1b
-    if (typeof props.children.type === 'function') {
-      children = React.cloneElement(props.children, {
-        closePortal: this.closePortal
-      });
-    }
+let children = props.children;
+// https://gist.github.com/jimfb/d99e0678e9da715ccf64549..
+if (typeof props.children.type === 'function') {
+children = React.cloneElement(props.children, {
+closePortal: closePortal
+});
+}
 
-    this.portal = ReactDOM.unstable_renderSubtreeIntoContainer(
-      this,
-      children,
-      this.node,
-      this.props.onUpdate
-    );
-  }
+this.portal = ReactDOM.unstable_renderSubtreeIntoContainer(
+this,
+children,
+this.node,
+onUpdate
+);
+}
 
-  render() {
-    if (this.props.openByClickOn) {
-      return React.cloneElement(this.props.openByClickOn, {
-        onClick: this.handleWrapperClick
-      });
-    }
-    return null;
-  }
+render() {
+const { openByClickOn } = this.props;
+if (openByClickOn) {
+return React.cloneElement(openByClickOn, {
+onClick: this.handleWrapperClick
+});
+}
+return null;
+}
 }
 
 Portal.propTypes = {
-  children: PropTypes.element.isRequired,
-  openByClickOn: PropTypes.element,
-  closeOnEsc: PropTypes.bool,
-  closeOnOutsideClick: PropTypes.bool,
-  isOpen: PropTypes.bool,
-  isOpened: (props, propName, componentName) => {
-    if (typeof props[propName] !== 'undefined') {
-      return new Error(
-        `Prop \`${propName}\` supplied to \`${componentName}\` was renamed to \`isOpen\`.
-          https://github.com/tajo/react-portal/pull/82.`
-      );
-    }
-    return null;
-  },
-  onOpen: PropTypes.func,
-  onClose: PropTypes.func,
-  beforeClose: PropTypes.func,
-  onUpdate: PropTypes.func
+children: PropTypes.element.isRequired,
+openByClickOn: PropTypes.element,
+closeOnEsc: PropTypes.bool,
+closeOnOutsideClick: PropTypes.bool,
+isOpen: PropTypes.bool,
+isOpened: (props, propName, componentName) => {
+if (typeof props[propName] !== 'undefined') {
+return new Error(
+`Prop \`${propName}\` supplied to \`${componentName}\` was renamed to \`isOpen\`.
+https://github.com/tajo/react-portal/pull/82.`
+);
+}
+return null;
+},
+onOpen: PropTypes.func,
+onClose: PropTypes.func,
+beforeClose: PropTypes.func,
+onUpdate: PropTypes.func
 };
 
 Portal.defaultProps = {
-  onOpen: () => {},
-  onClose: () => {},
-  onUpdate: () => {}
+onOpen: () => {},
+onClose: () => {},
+onUpdate: () => {}
 };
