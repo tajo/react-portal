9d5217bf5d3c0d5dce5368fc04625e5545a08ded
Tim van der Horst <timgvdh@gmail.com>
2017-11-02 14:43:23 +1000

    Failing tests for the desired dynamic prop changes to closeOnEsc and closeOnOutsideClick

--- __tests__/PortalWithState.js
+++ __tests__/PortalWithState.js
@@ -97,6 +97,46 @@ test('should close portal after escape key is pressed with closeOnEsc', () => {
   );
 });
 
+test('should not close portal after escape key is pressed with closeOnEsc changed to false', () => {
+  const childrenFunc = ({ portal }) => portal('Foo');
+  ReactDOM.render(
+    <PortalWithState defaultOpen closeOnEsc>
+      {childrenFunc}
+    </PortalWithState>,
+    document.getElementById('root')
+  );
+  ReactDOM.render(
+    <PortalWithState defaultOpen>{childrenFunc}</PortalWithState>,
+    document.getElementById('root')
+  );
+  expect(document.body.firstChild.outerHTML).toBe('<div id="root"></div>');
+  expect(document.body.lastChild.outerHTML).toBe('<div>Foo</div>');
+  const event = new KeyboardEvent('keydown', { keyCode: KEYCODES.ESCAPE });
+  document.dispatchEvent(event);
+  expect(document.body.lastChild.outerHTML).toBe('<div>Foo</div>');
+});
+
+test('should close portal after escape key is pressed with closeOnEsc changed to true', () => {
+  const childrenFunc = ({ portal }) => portal('Foo');
+  ReactDOM.render(
+    <PortalWithState defaultOpen>{childrenFunc}</PortalWithState>,
+    document.getElementById('root')
+  );
+  ReactDOM.render(
+    <PortalWithState defaultOpen closeOnEsc>
+      {childrenFunc}
+    </PortalWithState>,
+    document.getElementById('root')
+  );
+  expect(document.body.firstChild.outerHTML).toBe('<div id="root"></div>');
+  expect(document.body.lastChild.outerHTML).toBe('<div>Foo</div>');
+  const event = new KeyboardEvent('keydown', { keyCode: KEYCODES.ESCAPE });
+  document.dispatchEvent(event);
+  expect(document.body.lastChild.outerHTML).toBe(
+    document.body.firstChild.outerHTML
+  );
+});
+
 test('should close portal after outside click with closeOnOutsideClick', () => {
   ReactDOM.render(
     [
@@ -105,6 +145,74 @@ test('should close portal after outside click with closeOnOutsideClick', () => {
       </button>,
       <PortalWithState key="portal" defaultOpen closeOnOutsideClick>
         {({ portal }) => portal('Foo')}
+      </PortalWithState>
+    ],
+    document.getElementById('root')
+  );
+  expect(document.body.firstChild.outerHTML).toBe(
+    '<div id="root"><button id="trigger">Outside</button></div>'
+  );
+  expect(document.body.lastChild.outerHTML).toBe('<div>Foo</div>');
+  document.getElementById('trigger').click();
+  expect(document.body.lastChild.outerHTML).toBe(
+    document.body.firstChild.outerHTML
+  );
+});
+
+test('should not close portal after outside click with closeOnOutsideClick changed to false', () => {
+  const button = (
+    <button key="trigger" id="trigger">
+      Outside
+    </button>
+  );
+  const childrenFunc = ({ portal }) => portal('Foo');
+  ReactDOM.render(
+    [
+      button,
+      <PortalWithState key="portal" defaultOpen closeOnOutsideClick>
+        {childrenFunc}
+      </PortalWithState>
+    ],
+    document.getElementById('root')
+  );
+  ReactDOM.render(
+    [
+      button,
+      <PortalWithState key="portal" defaultOpen>
+        {childrenFunc}
+      </PortalWithState>
+    ],
+    document.getElementById('root')
+  );
+  expect(document.body.firstChild.outerHTML).toBe(
+    '<div id="root"><button id="trigger">Outside</button></div>'
+  );
+  expect(document.body.lastChild.outerHTML).toBe('<div>Foo</div>');
+  document.getElementById('trigger').click();
+  expect(document.body.lastChild.outerHTML).toBe('<div>Foo</div>');
+});
+
+test('should close portal after outside click with closeOnOutsideClick changed to true', () => {
+  const button = (
+    <button key="trigger" id="trigger">
+      Outside
+    </button>
+  );
+  const childrenFunc = ({ portal }) => portal('Foo');
+  ReactDOM.render(
+    [
+      button,
+      <PortalWithState key="portal" defaultOpen>
+        {childrenFunc}
+      </PortalWithState>
+    ],
+    document.getElementById('root')
+  );
+  ReactDOM.render(
+    [
+      button,
+      <PortalWithState key="portal" defaultOpen closeOnOutsideClick>
+        {childrenFunc}
       </PortalWithState>
     ],
     document.getElementById('root')
