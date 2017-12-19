4a500c6cd3a196e054b4977649e7b350c3c278b6
Alex Ketch <alex-ketch@users.noreply.github.com>
2017-12-19 00:04:48 -0500

    Fix typo

--- src/PortalWithState.js
+++ src/PortalWithState.js
@@ -72,7 +72,7 @@ class PortalWithState extends React.Component {
     if (!this.state.active) {
       return;
     }
-    const root = this.portalNode.props.node || this.protalNode.defaultNode;
+    const root = this.portalNode.props.node || this.portalNode.defaultNode;
     if (!root || root.contains(e.target) || (e.button && e.button !== 0)) {
       return;
     }
